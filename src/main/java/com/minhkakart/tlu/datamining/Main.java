984ef1cd632577afe30f48d614d7b3d4cac9ffbb
minhkakart <nguyenminh2732@gmail.com>
2024-10-20 21:14:48 +0700

    end pre-test

--- src/main/java/com/minhkakart/tlu/datamining/Main.java
+++ src/main/java/com/minhkakart/tlu/datamining/Main.java
@@ -1,73 +1,23 @@
 package com.minhkakart.tlu.datamining;
 
-import com.minhkakart.tlu.datamining.associationrule.AprioriTid;
 import com.minhkakart.tlu.datamining.clustering.KMeans;
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvException;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
 
-@SuppressWarnings("ExtractMethodRecommender")
 public class Main {
     public static void main(String[] args) throws IOException, CsvException {
-        /* Data
-        List<Set<String>> transactions = Arrays.asList(
-                new HashSet<>(Arrays.asList("b e f g h".split(" "))),
-                new HashSet<>(Arrays.asList("d f k m".split(" "))),
-                new HashSet<>(Arrays.asList("a c d f h m n".split(" "))),
-                new HashSet<>(Arrays.asList("a b h k m n".split(" "))),
-                new HashSet<>(Arrays.asList("a b e g h m n".split(" ")))
-        );
-
-        double minSupport = 0.6;
-        double minConfidence = 0.8;*/
-
-        /* Apriori
-        Apriori apriori = new Apriori(minSupport, minConfidence);
-        List<Set<String>> frequentItemSets = apriori.findFrequentItemSets(transactions);
-        List<String> associationRules = apriori.generateAssociationRules(frequentItemSets);
-
-        System.out.println("Frequent ItemSets:");
-        for (Set<String> itemset : frequentItemSets) {
-            System.out.println(itemset);
-        }
-
-        System.out.println("\nAssociation Rules:");
-        for (String rule : associationRules) {
-            System.out.println(rule);
-        }*/
-
-        /* AprioriTid
-        AprioriTid aprioriTid = new AprioriTid(minSupport);
-        List<Set<String>> frequentItemSets = aprioriTid.findFrequentItemSets(transactions);
-
-        System.out.println("Frequent ItemSets:");
-        for (Set<String> itemset : frequentItemSets) {
-            System.out.println(itemset);
-        }*/
-
-        /* FpTree
-        FpTree fpTree = new FpTree(minSupport);
-        fpTree.buildTree(transactions);
-        List<Set<String>> frequentItemsets = fpTree.mineFrequentItemsets();
-
-        System.out.println("Frequent Itemsets:");
-        for (Set<String> itemset : frequentItemsets) {
-            System.out.println(itemset);
-        }*/
-
-        // Clustering
+        
         long overallStartTime = System.currentTimeMillis();
 
         System.out.println("Reading data...");
-        List<String[]> rawData = new CSVReader(new FileReader("F:\\linh_tinh\\tesst123.csv")).readAll();
+        List<String[]> rawData = new CSVReader(new FileReader("F:\\data\\data.csv")).readAll();
         rawData = rawData.subList(1, rawData.size());
 
         int k = 2;
